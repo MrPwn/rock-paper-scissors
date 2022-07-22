5c088518bc16c22049d054ea4e83d440626cbcc7
David Adeyeye <dadeyeye98@gmail.com>
2022-07-21 21:02:25 -0500

    Implement game logic to pick a winner

--- myScript.js
+++ myScript.js
@@ -10,7 +10,35 @@ function getComputerChoice() {
         choice = 'Scissors';
     }
     
-    console.log(choice);
+    return choice.toLowerCase();
 }
 
-getComputerChoice();
+function playSingleRound(playerSelection, computerSelection) {
+    let playerLower = playerSelection.toLowerCase();
+    let winMessage = '';
+
+    if (computerSelection === playerLower) {
+        return winMessage = 'It\'s a tie!';
+    }
+    
+    switch (computerSelection) {
+
+        case 'rock':
+            winMessage = (playerLower === 'scissors') ? 'pc wins' : 'you win';
+            break;
+        
+        case 'scissors':
+            winMessage = (playerLower === 'paper') ? 'pc wins' : 'you win';
+            break;
+        
+        case 'paper':
+            winMessage = (playerLower === 'rock') ? 'pc wins' : 'you win';
+            break;
+    }
+    
+    console.log(computerSelection);
+    console.log(winMessage);
+}
+const playerSelection = 'Rock';
+
+playSingleRound(playerSelection, getComputerChoice());
